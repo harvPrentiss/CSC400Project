e690d2df1fde178678387cfff57639db06f08b47
Harold Prentiss <harv.prentiss@gmail.com>
2014-02-16 15:52:24 -0500

    Added static skeleton pages

--- fitness_app/spec/requests/static_pages_spec.rb
+++ fitness_app/spec/requests/static_pages_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-  	it "should have the content 'Fitness App'" do
+  	it "should have the content 'Pulse Fitness'" do
   		visit '/static_pages/home'
-  		expect(page).to have_content('Fitness App')
+  		expect(page).to have_content('Pulse Fitness')
   	end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Pulse Fitness | Home")
+    end
   end
 
   describe "Help page" do
@@ -13,6 +17,21 @@ describe "StaticPages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Pulse Fitness | Home")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Pulse Fitness | Home")
+    end
   end
 end
 
