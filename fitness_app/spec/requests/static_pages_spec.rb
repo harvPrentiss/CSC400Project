10c806da97c46acdc53b874d5bd2128206aac106
Harold Prentiss <harv.prentiss@gmail.com>
2014-02-17 16:29:36 -0500

    Incorporated in my CSS
    Its not working as expected. Need to get the header css working then
    move on to more work.

--- fitness_app/spec/requests/static_pages_spec.rb
+++ fitness_app/spec/requests/static_pages_spec.rb
@@ -6,9 +6,9 @@ describe "StaticPages" do
   		visit '/static_pages/home'
   		expect(page).to have_content('Pulse Fitness')
   	end
-    it "should have the title 'Home'" do
+    it "should have the base_title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Pulse Fitness | Home")
+      expect(page).to have_title("Pulse Fitness")
     end
   end
 
@@ -17,9 +17,9 @@ describe "StaticPages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
-    it "should have the title 'Home'" do
+    it "should have the title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Pulse Fitness | Home")
+      expect(page).to have_title("Pulse Fitness")
     end
   end
 
@@ -28,9 +28,9 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
-    it "should have the title 'Home'" do
+    it "should have the title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Pulse Fitness | Home")
+      expect(page).to have_title("Pulse Fitness")
     end
   end
 end
