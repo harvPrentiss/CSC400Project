3f5467f927a28de8b64a6ad713345e04656df979
Harold Prentiss <harv.prentiss@gmail.com>
2014-03-04 22:07:49 -0500

    Got signup working and showing basic info
    Need to do some styling when get a chance

--- fitness_app/app/controllers/users_controller.rb
+++ fitness_app/app/controllers/users_controller.rb
@@ -5,7 +5,24 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			#successful save
+			flash[:success] = "Welcome to Pulse Fitness!"
+			redirect_to @user
+		else
+			# show errors and try again
+			render 'new'
+		end
+	end
+
+	private
   
+  		def user_params
+  			params.require(:user).permit(:displayName, :emailAdd, :password, :password_confirmation)
+  		end
 end
