b67365ca6201a108ddae4194f1953e3b5f11b6f0
Harold Prentiss <harv.prentiss@gmail.com>
2014-03-23 23:59:58 -0400

    Lot of styling and a bonus
    Lots of styling done to the home page when logged in everyhting fits and
    is organized. Also added a character counter when composing updates.
    
    Still need to style users, following, followers, and things being paged.
    still need the main profile page and also need to get profile images
    working.

--- fitness_app/app/assets/javascripts/application.js
+++ fitness_app/app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+var ready;
+var MAX_CHARACTERS = 140;
+ready = function(){
+	$('.statusPostBox').keyup(function(){
+		$('.characterCount').removeClass('redWarning');
+		$('.characterCount').text(MAX_CHARACTERS - this.value.replace(/{.*}/g, '').length);
+		if(MAX_CHARACTERS - this.value.replace(/{.*}/g, '').length <= 20){
+			$('.characterCount').addClass('redWarning');
+		}
+	})
+
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
