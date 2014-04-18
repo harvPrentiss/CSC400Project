148042192b720bde02249834ebb87bae4b32caf3
Harold Prentiss <harv.prentiss@gmail.com>
2014-04-18 18:32:13 -0400

    Index?

--- fitness_app/db/schema.rb
+++ fitness_app/db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20140412001536) do
     t.datetime "updated_at"
   end
 
+  add_index "statuses", ["user_id", "created_at"], name: "index_statuses_on_user_id_and_created_at"
+
   create_table "users", force: true do |t|
     t.string   "displayName"
     t.string   "password"
