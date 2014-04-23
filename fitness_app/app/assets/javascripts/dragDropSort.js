bda6a96741d8e19b39edea9aa01685c4bbb28260
Harold Prentiss <harv.prentiss@gmail.com>
2014-04-23 00:09:20 -0400

    Not much added
    Drag and drop is almost ready to go.

---
+++ fitness_app/app/assets/javascripts/dragDropSort.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){	
+
+	// Makes the lists to add and remove exercises from routines sortable, drag and droppable.
+	$('.userExerciseList').sortable({
+		connectWith: '.routineExerciseList',
+		placeholder: 'placeholder',
+	});
+
+
+
+});)
